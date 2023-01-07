6ac21aca0777724a49450ef952c35797f5a917a5
saieesh1997 <saieesh.s.naik@gmail.com>
2023-01-07 19:00:03 +0530

    Javascript folder created inside live class folder and first javascript 10thdec class completed

---
+++ Live_Classes/JAVASCRIPT/10thDec_live_class/classone.js
@@ -0,0 +1,107 @@
+// 7;   //whole number
+// 2.5; //decimal number
+// //Number values
+
+// ("anurag");
+// ('anurag');
+// // either in single quote or double quote it is string only(string values)
+
+// true;
+// false;
+
+// //true or false are called as boolean values
+
+// null;;
+// undefined;
+// //Empty values they dont hold any type of values inside them.
+
+// //ALL the above ones are called as primitve values or primitive datatype= single values
+
+// //-------------------------------------------------------------------------------------------------//
+
+
+// //Array and Object
+
+// [1,2,3,4,5,6]
+
+// [1,'saieesh',true,"anurag"]  //can have any type in a array
+
+// //Objects
+// {
+//     key:value
+// }
+
+// {
+//     fname:saieesh,
+//     lname:Naik
+// }
+
+// //The above two are non primitve datatype=more than one values
+
+
+//Variables - place holder for values
+//3 ways to declare variable
+// var y = 25;
+// let x = 25;
+// const z= "Anurag"; //constant -that doesnot change for over a time
+
+//simple suggestion never use var use let and const.
+
+// let firstname= "Anurag"
+// let lastname="tiwari"
+// const mobileno= 990183457;
+
+// console.log("My first name is: ",firstname);
+// console.log("My last name is: ",lastname);
+// console.log(mobileno);
+
+//Template literal
+
+// console.log(`My Current First Name is ${firstname}`);
+// console.log(`My Current First Name is ${firstname} ${lastname} ${mobileno}`);
+
+//Operators (operation simply means calculation)
+//Assignment operator
+// let x = 10;
+// let y = 10;
+
+//Airthmetic operator (+, -, *, /, (%->Remainder))
+// let z = x+y;
+// console.log(z);
+
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+
+//Comparision operator
+// let abc = 10;
+// let xyz = '10';
+
+// console.log(abc > xyz); //greater than
+// console.log(abc < xyz); //less than
+// console.log(abc >= xyz); //either greater or same
+
+// == or ===
+// console.log(abc === xyz);
+
+// let ya;
+
+// console.log(ya);
+
+// const accno = "5012315";
+// console.log(accno);
+
+
+//Decisions or conditions
+
+// let age = 19;
+
+// if(age>=18){
+//     console.log('you are allowed inside the park');
+// }else{
+//     console.log('out of the park');
+// }
+
+
